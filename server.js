605da6b4ee397652914735aea2f4dd97458df3ca
Thomas Rory Gummerson <thomas@gummerson.no>
2019-01-02 12:52:41 +0100

    Done

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 require('dotenv').config();
-const Telegraf = require('telegraf');
-const request = require('request');
-const { CronJob } = require('cron');
+const axios = require('axios');
 const { createHash } = require('crypto');
 const { JSDOM } = require('jsdom');
 const mongoose = require('mongoose');
+const request = require('request');
+const Telegraf = require('telegraf');
 
-const { Schema, model } = mongoose;
+const { env } = process;
+const { Schema, model, Types: { ObjectId } } = mongoose;
 
-const people = process.env.PEOPLE
-	? process.env.PEOPLE.split(',')
-	: [ 'Akash' ];
-const admin = process.env.ADMIN
-	? Number(process.env.ADMIN)
-	: 475757469;
-const token = process.env.BOT_TOKEN;
+const people = env.PEOPLE ? env.PEOPLE.split(',') : [ 'Akash' ];
+const admin = env.ADMIN ? Number(env.ADMIN) : 475757469;
+const updateInterval = env.UPDATE_INTERVAL || 900;
+const maxAge = env.MAX_AGE || 900;
+const mongoURL = env.MONGO_URI || 'mongodb://localhost:27017/SiteSeer';
+const token = env.BOT_TOKEN;
+const debug = Boolean(env.DEBUG);
+const ads = Boolean(env.ADS);
+
+if (!token) {
+	throw new Error('Missing BOT_TOKEN env var');
+}
+
 
 //Function to calculate checksum using crypto
 const checksum = input =>
@@ -22,10 +29,11 @@ const checksum = input =>
 
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+mongoose.connect(mongoURL, { useNewUrlParser: true });
 
 const SiteSchema = new Schema({
 	url: { type: String, required: true, unique: true },
+	checked: { type: Date, required: true, default: Date.now },
 	checksum: { type: String }
 });
 const Site = model('Site', SiteSchema);
@@ -45,14 +53,69 @@ const Chat = model('Chat', ChatSchema);
 const ensureSiteExists = url =>
 	Site.findOneAndUpdate(
 		{ url },
-		{ $set: { url } },
-		{ upsert: true, new: true });
+		{ $set: { url, checked: Date.now() } },
+		{ upsert: true, new: true, setDefaultsOnInsert: true });
 
 const ensureChatExists = id =>
 	Chat.findOneAndUpdate(
 		{ id },
 		{ $set: { id } },
-		{ upsert: true, new: true });
+		{ upsert: true, new: true, setDefaultsOnInsert: true });
+
+const ensureExists = (url, id, populate = false) =>
+	Promise.all([
+		ensureSiteExists(url),
+		populate
+			? ensureChatExists(id).populate('sites')
+			: ensureChatExists(id)
+	]);
+
+const urlFromMessage = text => {
+	const arg = text.split(' ').slice(1).join(' ').trim();
+	return (/^http(s)?:\/\//).test(arg) ? arg : `http://${arg}`;
+};
+
+const messageAll = async (site_id, message) => {
+	for await (const chat of Chat.find({ sites: site_id })) {
+		await bot.telegram.sendMessage(chat.id, message);
+	}
+};
+
+const siteWatcher = url =>
+	axios(url)
+		.then(res => res.data)
+		.then(checksum);
+
+const checkSite = site =>
+	siteWatcher(site.url)
+		.then(checksum =>
+			Promise.all([
+				site.updateOne({ $set: { checksum, checked: Date.now() } }),
+				messageAll(site._id, [
+					`The site, ${site.url}, might have changed!`,
+					...ads
+						? [
+							'Support me here :\nhttp://m.p-y.tm/requestPayment?recipient=8669091448',
+							'OR',
+							'ko-fi.com/akashjoshi'
+						]
+						: []
+				].join('\n\n'))
+			]))
+		.catch(err =>
+			Promise.all([
+				site.updateOne({ $set: { checked: Date.now() } }),
+				messageAll(site._id, `The site, ${site.url}, is down!` +
+					(debug ? ' (' + err.message + ')' : ''))
+			]));
+
+const batchWatch = async () => {
+	for await (const site of Site.find({
+		checked: { $lte: Date.now() - (maxAge * 1000) }
+	})) {
+		await checkSite(site);
+	}
+};
 
 const bot = new Telegraf(token);
 
@@ -68,24 +131,19 @@ Note : Doesn't work for dynamic sites like Instagram or Facebook.
 Your chatid is ${ctx.chat.id}`));
 
 // Matches "/echo [whatever]"
-bot.command('watch', ({ chat: { id }, message, reply }) => {
-	const arg = message.text.split(' ').slice(1).join(' ').trim();
-	const url = (/^http(s)?:\/\//).test(arg) ? arg : `http://${arg}`;
-	return Promise.all([
-		ensureSiteExists(url),
-		ensureChatExists(id)
-	])
+bot.command('watch', ({ chat: { id }, message: { text }, reply }) =>
+	ensureExists(urlFromMessage(text), id)
 		.then(([ site, chat ]) =>
 			[ site, chat, chat.sites.some(x => x.equals(site.id)) ])
 		.then(([ site, chat, chatHasSite ]) =>
 			chatHasSite
 				? reply('Already subscribed')
 				: chat.updateOne(
-					{ $push: { sites: site.id } },
+					{ $push: { sites: site._id } },
 					{ new: true })
 					.then(() =>
-						reply(`Checking ${url} for you!`)));
-});
+						reply(`Checking ${site.url} for you!`))
+					.then(() => checkSite(site))));
 
 bot.command('list', ({ chat: { id }, reply }) =>
 	ensureChatExists(id).populate('sites')
@@ -95,104 +153,26 @@ bot.command('list', ({ chat: { id }, reply }) =>
 				'\n\nUse /watch sitename " to subscribe to notifs of that site' +
 				'\n\nUse /unsub sitename to unsubscribe')));
 
-bot.command('unsub', (msg, match) => {
-	if(siteList.indexOf(match[1]) != -1){
-		Promise.all(sites.map((element)=>{
-			if(element.url == match[1]){
-				element.chatId = element.chatId.filter((value)=>{
-					return value != msg.chat.id;
-				})
-				bot.telegram.sendMessage(msg.chat.id,`If you were subscribed to ${match[1]}, you no longer are`)
-				return true;
-			}
-			return false
-		}))
-	}
-	else bot.telegram.sendMessage(msg.chat.id,`${match[1]} isn't a valid site. Please check /list for available websites`)
-})
+bot.command('unsub', ({ chat: { id }, message: { text }, reply }) => {
+	const url = urlFromMessage(text);
+	return ensureExists(url, id, true)
+		.then(([ site, chat ]) =>
+			[ site, chat, chat.sites.find(x => x.url === site.url) ])
+		.then(([ site, chat, siteToRemove ]) =>
+			siteToRemove && chat.updateOne(
+				{ $pull: { sites: siteToRemove._id } }))
+		.then(res =>
+			reply(
+				res
+					? `If you were subscribed to ${url}, you no longer are`
+					: `"${url}" isn't a valid site. Please check /list for available websites`));
+});
 
-var job = new CronJob('*/15 * * * *', batchWatch//()=>{console.log(1)}
-, function endCronJob(){
-    console.log('cronJob ended')
-  },true,
-  'America/Los_Angeles' /* Time zone of this job. */
-);
+batchWatch();
 
-function batchWatch (){
-	Promise.all(sites.map((element)=>{
-		siteWatcher(element)
-	}))
-	const newData = {
-		'sites' : sites,
-		"_id": "5c2c4461e7179a49f40abe2d"
-	}
-	myModel.findOneAndUpdate(query, newData, {upsert:true}, function(err){
-		if (err) return console.log(500, { error: err });
-		return console.log("succesfully saved");
-	});
-}
-
-// Watch the site for changes...
-function siteWatcher(siteObject){
-	let userMessages = {
-		"SITE_HAS_CHANGED": `The site, ${siteObject.url}, might have changed!
-
-		Support me here :
-		http://m.p-y.tm/requestPayment?recipient=8669091448
-
-		OR
-
-		ko-fi.com/akashjoshi`,
-        "SITE_IS_DOWN": `The site, ${siteObject.url}, is down!`
-	}
-
-	// Check to see if there is a seed checksum
-	if(siteObject.checksumString == ''){
-
-		// Create the first checksum and return
-		return request(siteObject.url, function initialRequestCallback(error, response, body){
-
-			if(error){return console.error(error)}
-
-			if(response.statusCode < 400){
-				const dom = new JSDOM(body)
-				return siteObject.checksumString = checksum(dom.window.document.querySelector('body').textContent.trim())
-            } 
-            else{
-				Promise.all(siteObject.chatId.map((element1)=>bot.telegram.sendMessage(element1,userMessages.SITE_IS_DOWN)
-				))
-			} 
-		}) // end request
-	}
-	else{
-		// Compare current checksum with latest request body 
-		return request(siteObject.url, function recurringRequestCallback(error, response, body){
-
-			if(error){return console.error(error)}
-
-			if(response.statusCode < 400){
-				
-				const dom = new JSDOM(body)
-				let currentCheckSum = checksum(dom.window.document.querySelector('body').textContent.trim())
-				
-				if(siteObject.checksumString != currentCheckSum){
-					// They are not the same so send notification 
-					
-					// Update checkSumString's value
-					siteObject.checksumString = currentCheckSum
-
-					Promise.all(siteObject.chatId.map((element1)=>bot.telegram.sendMessage(element1,userMessages.SITE_HAS_CHANGED)
-					))
-				}
-				// else site still same
-            }
-            else  {
-				Promise.all(siteObject.chatId.map((element1)=>bot.telegram.sendMessage(element1,userMessages.SITE_IS_DOWN)
-				))
-			} 
-		})
-	} 
-}
+setInterval(() => {
+	batchWatch();
+}, updateInterval * 1000);
 
 bot.catch(console.error);
 
